b0ca4572985cdab0dbb3abcd906ed7a39722022f
First commit <novodolskij@gmail.com>
2025-12-07 01:36:09 +0100

    Initial commit: Add accuracy counter and game-over system

--- Аим.py
+++ Аим.py
@@ -4,22 +4,24 @@ import math
 
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("Aim Trainer — шаг 3: рекорды + попадания")
+pygame.display.set_caption("Aim Trainer")
 
 x = 400
 y = 300
 radius = 40
+
 score = 0
+shots = 0
+hits = 0
+accuracy = 0
 
 font = pygame.font.SysFont("None", 36)
-
 
 try:
     with open("Records.txt", "r") as f:
         best_score = int(f.read().strip() or 0)
-except (FileNotFoundError, ValueError):
+except:
     best_score = 0
-
 
 running = True
 while running:
@@ -28,35 +30,47 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-        # клик мышкой
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
+                shots += 1
                 mx, my = event.pos
 
                 dist = math.dist((mx, my), (x, y))
 
                 if dist < radius:
+                    hits += 1
                     score += 1
+
+                    if score > best_score:
+                        best_score = score
+
                     x = random.randint(radius, 800 - radius)
                     y = random.randint(radius, 600 - radius)
 
-
     screen.fill((30, 30, 30))
-
 
     pygame.draw.circle(screen, (255, 60, 60), (x, y), radius)
 
-    #текст очков
     score_text = font.render(f"Score: {score}", True, (255, 255, 255))
     screen.blit(score_text, (10, 10))
 
     record_text = font.render(f"Record: {best_score}", True, (200, 200, 200))
     screen.blit(record_text, (10, 40))
 
+    if shots > 0:
+        accuracy = hits / shots * 100
+    else:
+        accuracy = 0
+
+    acc_text = font.render(f"Acc: {accuracy:.1f}%", True, (180, 180, 255))
+    screen.blit(acc_text, (10, 150))
+
     pygame.display.flip()
 
 if score > best_score:
-    with open("Records.txt", "w") as f:
-        f.write(str(score))
+    best_score = score
+
+with open("Records.txt", "w") as f:
+    f.write(str(best_score))
 
 pygame.quit()
